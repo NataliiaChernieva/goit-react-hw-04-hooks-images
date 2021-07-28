9cfd93fac870a237cd960ca9cfb2ae5d00e58ebf
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-07-28 18:55:17 +0300

    добавляет src

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,11 @@
+import { CustomSearchbar } from './Searchbar.styled';
+import  SearchForm  from '../SearchForm/SearchForm';
+
+export default function Searchbar ({onSubmit}) {
+    return(
+        <CustomSearchbar>
+            <SearchForm onSubmit={onSubmit}/>
+        </CustomSearchbar>
+    )
+}
+
